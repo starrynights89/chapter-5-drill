a43327e9b721c8bb1e7ef18809993a8990a99422
starrynights89 <alexander-hartson@live.com>
2017-11-03 13:31:33 -0500

    adding code

--- scaffolding.cpp
+++ scaffolding.cpp
@@ -9,5 +9,13 @@ try
 }
 catch (exception& e)
 {
-    cerr << "error: " 
+    cerr << "error: " << e.what() << '\n';
+    keep_window_open();
+    return 1;
+}
+catch (...)
+{
+    cerr << "Oops: unknown exception!\n";
+    keep_window_open();
+    return 2; 
 }
