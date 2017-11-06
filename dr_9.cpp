1550861fc00b5e3000306d14c958a0772542e6c8
starrynights89 <alexander-hartson@live.com>
2017-11-06 13:33:05 -0600

    bug fix

--- dr_9.cpp
+++ dr_9.cpp
@@ -3,7 +3,10 @@
 int main()
 try
 {
-    string s = "ape"; boo c = "fool"<s; if (c) cout << "Success!\n";
+    string s = "ape"; bool c = "fool">=s;
+    if (c) cout << "Success!\n";
+    else 
+        cout << "Fail!\n";
     keep_window_open();
     return 0;
 }
