85e23caacb3615aa76043001827de895410dc2e0
starrynights89 <alexander-hartson@live.com>
2017-11-06 15:19:22 -0600

    bug fix

--- dr_19.cpp
+++ dr_19.cpp
@@ -3,7 +3,9 @@
 int main()
 try
 {
-    vector v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+    vector<int> v(5);
+	for (int i=0; i<v.size(); ++i);
+		cout << "Success!\n";
     keep_window_open();
     return 0;
 }
