b57e2fa33b2a96988cbecd8d39010bb8ca1c61d4
starrynights89 <alexander-hartson@live.com>
2017-11-06 15:42:56 -0600

    creating file

--- code-fragments.cpp
+++ code-fragments.cpp
@@ -18,7 +18,7 @@
 //string s = "Success!\n"; for (int i=0; i<10; ++i) cout << s[i];
 //vector v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
 //int i=0; int j = 9; while (i<10) ++j; if (j<i) cout << "Success!\n";
-int x = 2; double d = 5/(x–2); if (d==2*x+0.5) cout << "Success!\n";
+//int x = 2; double d = 5/(x–2); if (d==2*x+0.5) cout << "Success!\n";
 string<char> s = "Success!\n"; for (int i=0; i<=10; ++i) cout << s[i];
 int i=0; while (i<10) ++j; if (j<i) cout << "Success!\n";
 int x = 4; double d = 5/(x–2); if (d=2*x+0.5) cout << "Success!\n";
