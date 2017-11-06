67f03d52adb92d761a59062f3daa69c6b8d3b4a2
starrynights89 <alexander-hartson@live.com>
2017-11-06 12:51:20 -0600

    bug fix

--- dr_6.cpp
+++ dr_6.cpp
@@ -3,7 +3,12 @@
 int main()
 try
 {
-    vector<int> v(10); v[5] = 7; if (v[5]!=7) cout << "Success!\n";
+    vector<int> v(10);
+	v[5] = 7;
+	if (v[5] = 7)
+	{
+		cout << "Success!\n";
+	}
     keep_window_open();
     return 0;
 }
