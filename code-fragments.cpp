af278ab56762cc77cc72c1dc7d45ce812cb3fdec
starrynights89 <alexander-hartson@live.com>
2017-11-06 18:48:50 -0600

    adding comments

--- code-fragments.cpp
+++ code-fragments.cpp
@@ -21,5 +21,5 @@
 //int x = 2; double d = 5/(x–2); if (d==2*x+0.5) cout << "Success!\n";
 //string<char> s = "Success!\n"; for (int i=0; i<=10; ++i) cout << s[i];
 //int i=0; while (i<10) ++j; if (j<i) cout << "Success!\n";
-int x = 4; double d = 5/(x–2); if (d=2*x+0.5) cout << "Success!\n";
-cin << "Success!\n";
+//int x = 4; double d = 5/(x–2); if (d=2*x+0.5) cout << "Success!\n";
+//cin << "Success!\n";
